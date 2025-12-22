b0beb4a965ea35395ec9c196d733dacf79cf6755
valerio-pescatori <pescatorim14@gmail.com>
2025-12-22 14:07:25 +0100

    homepage, background, font

---
+++ app/components/Embla.tsx
@@ -0,0 +1,18 @@
+"use client";
+import clsx, { ClassValue } from "clsx";
+import useEmblaCarousel from "embla-carousel-react";
+import { JSX } from "react";
+
+export function EmblaCarousel({ slides, className }: { slides: JSX.Element[]; className?: ClassValue }) {
+  const [emblaRef] = useEmblaCarousel();
+
+  return (
+    <div className={clsx("overflow-hidden", className)} ref={emblaRef}>
+      <div className="flex">
+        {slides.map((slide, i) => (
+          <div key={`image-${i}`} className="min-w-0 flex-none basis-full">{slide}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,29 @@
 import Image from "next/image";
+import separator from "./assets/sep.png";
+import { EmblaCarousel } from "./components/Embla";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <main className="flex min-h-screen text-3xl w-full max-w-3xl flex-col items-center py-16 px-8 sm:items-start">
+        <h1 className="text-7xl">NAT</h1>
+        <div className="text-center mt-4">
+          <p>
+            Tattoo artist with five years of experience, specialized in realistic and micro-realistic tattoos, based in
+            Rome.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          {/* PHILOSPHY & SHOWCASE */}
+          <Image src={separator} alt="separator" />
+          <section className="flex gap-4 mt-4">
+            <div className="text-xl w-1/2">
+              <h3 className="text-3xl">Tattoo philosophy</h3>
+              <p className="mt-2">
+                Precision and respect: every tattoo reflects my artistic dedication and an impeccable professional work
+                ethic.
+              </p>
+            </div>
+            <EmblaCarousel className="border rounded h-48 w-1/2" slides={[<div>1</div>, <div>2</div>]}></EmblaCarousel>
+          </section>
         </div>
       </main>
     </div>
